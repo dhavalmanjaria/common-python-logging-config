58e724e6ac29558d0935a64fc7b49b93fd5a7b7c
Dhaval Anjaria <dhv2712@gmail.com>
2018-11-21 17:58:20 +0530

    Initial commit

---
+++ config.py
@@ -0,0 +1,47 @@
+
+# Logging
+LOGGING_CONFIG = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'default': {
+            'format': '[%(filename)s:%(lineno)s - %(funcName)s()] %(message)s',
+        },
+        'info': {
+            'format': '[%(asctime)s]: %(message)s',
+            'datefmt': '%H:%M:%S'
+        }
+    },
+    'handlers': {
+        'file': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'trademapper.log'
+        },
+        'debugfilehandler': {
+            'level': 'DEBUG',
+            'class': 'logging.FileHandler',
+            'filename': 'app.log',
+            'formatter': 'default'
+        },
+        'consolehandler': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'info'
+        }
+    },
+    'loggers': {
+        'app': {
+            'handlers': ['debugfilehandler', 'consolehandler'],
+            'level': 'DEBUG',
+            'propogate': True,
+        },
+        'console': {
+            'handlers': ['consolehandler', 'debugfilehandler'],
+            'level': 'INFO',
+            'propogate': True
+        }
+    }
+}
+
+dictConfig(LOGGING_CONFIG)
